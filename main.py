22c77df63928fcaf425f3ddb4b0443bf218d7549
Lluis12 <pokelluis@gmail.com>
2024-04-24 13:25:24 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
+diccionari = {}
 opcio1=0
+import os
+clear = lambda: os.system('cls')
 
 print("\n----------------------------------------")
 print("Benvingut a El Llibre de les Acceptions.")
@@ -12,3 +15,19 @@ print("3.Modificar")
 print("4.Eliminar")
 while opcio1 < 0 and opcio1 > 4:
     opcio1 = input("Introdueix una opcio valida")
+
+
+match :
+    case 1:
+        return "Has escollit Afegir"
+        cls()
+        clau = input("Introdueix una paraula:")
+        valor = input("Introdueix el seu significat")
+    case 2:
+        return "Has escollit Mostrar"
+    case 3:
+        return "Has escollit Modificar"
+    case 4:
+        return "Has escollit Eliminar"
+    default:
+        return "desconocido"
