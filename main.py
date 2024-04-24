58baa84b55c7a3fa3c0e4c0619733f84cb7b85d8
Lluis12 <pokelluis@gmail.com>
2024-04-24 12:56:49 +0200

    v1.0

---
+++ main.py
@@ -0,0 +1,14 @@
+opcio1=0
+
+print("\n----------------------------------------")
+print("Benvingut a El Llibre de les Acceptions.")
+print("----------------------------------------\n\n")
+
+#MenuOpcions
+print("Que vols fer? \n\n")
+print("1.Afegir")
+print("2.Mostrar")
+print("3.Modificar")
+print("4.Eliminar")
+while opcio1 < 0 and opcio1 > 4:
+    opcio1 = input("Introdueix una opcio valida")
